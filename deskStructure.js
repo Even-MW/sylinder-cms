5022f1c391330bfea2661218ec39f62715eb9a25
Even <even.mauland-hus@sylinder.no>
2022-02-04 15:42:08 +0100

    Added deskStructure configuration for multi tenat solution in one dataset.

--- deskStructure.js
+++ deskStructure.js
@@ -2,12 +2,16 @@ import { MdTapas, MdWeb } from 'react-icons/md'
 
 import Iframe from "sanity-plugin-iframe-pane";
 import S from '@sanity/desk-tool/structure-builder'
+import documentStore from 'part:@sanity/base/datastore/document'
+import { map } from 'rxjs/operators'
 import resolveProductionUrl from "./resolveProductionUrl";
 
 // We filter document types defined in structure to prevent
 // them from being listed twice
-const hiddenDocTypes = (listItem) =>
-    !["pageBuilder", "media.tag", "ingredient"].includes(listItem.getId());
+const hiddenDocTypes = (listItem) => {
+    console.log(listItem.spec.schemaType.fields)
+    return !["pageBuilder", "media.tag", "site"].includes(listItem.getId())
+};
 
 export const getDefaultDocumentNode = () => {
     // Return all documents with just 1 view: the form
@@ -21,27 +25,54 @@ export const getDefaultDocumentNode = () => {
             .title("Preview"),
     ])
 }
+
+const siteQuery = `*[_type == "site"]`
+
 export default () =>
-    S.list()
-        .title('Innhold')
-        .items(
-            [
-                S.listItem()
-                    .title('Sider')
-                    .icon(MdWeb)
-                    .child(
-                        S.documentTypeList('pageBuilder')
+    documentStore.listenQuery(siteQuery).pipe(
+        map((sites) =>
+            S.list()
+                .title('Innhold')
+                .items(
+                    [
+                        S.listItem()
                             .title('Sider')
-                    ),
-                S.divider(),
-                ...S.documentTypeListItems().filter(hiddenDocTypes),
-                S.divider(),
-                S.listItem()
-                    .title('Ingredienser')
-                    .icon(MdTapas)
-                    .child(
-                        S.documentTypeList('ingredient')
-                            .title('Ingredienser')
-                    ),
-            ]
-        )
+                            .icon(MdWeb)
+                            .child(
+                                S.documentTypeList('pageBuilder')
+                                    .title('Sider')
+                            ),
+                        S.divider(),
+                        ...S.documentTypeListItems().filter(hiddenDocTypes),
+                        S.divider(),
+                        S.listItem()
+                            .title('Ny side')
+                            .icon(MdWeb)
+                            .child(
+                                S.documentTypeList('site')
+                                    .title('Ny side')
+                            ),
+                        // For every new site create a new list item with access to all listItems
+                        ...sites.map((site) =>
+                            S.listItem()
+                                .title(site.name)
+                                .child((pageId) => {
+                                    console.log(pageId)
+                                    return S.documentTypeList('article')
+                                        .title('Artikkel')
+                                        .filter('_type == $type && $pageId in owners[].name')
+                                        .params({ type: 'article', pageId })
+                                }
+                                    // S.list()
+                                    //     .title(site.name)
+                                    //     .items([
+                                    //         ...S.documentTypeListItems()
+                                    //             .filter(hiddenDocTypes,)
+                                    //         // .filter('$pageId in owners[]._ref')
+                                    //         // .params({ pageId }),
+                                    //     ])
+                                )
+                        )
+                    ]
+                )
+        ))
