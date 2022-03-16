bf4fc5909091880f8c905c1f93c9bbb442785d03
Even <even.mauland-hus@noaignite.com>
2022-03-16 14:50:27 +0100

    Added check to have data before using it.

--- unil_web/components/articleBlock/articleBlock.js
+++ unil_web/components/articleBlock/articleBlock.js
@@ -1,15 +1,23 @@
 import styles from "../../styles/ArticleBlock.module.css"
 import { urlFor } from "../../lib/sanity"
 export default function ArticleBlock({ data }) {
+
+    if (!data)
+        return null
+
+
     return (
         <article className={styles.main} style={{ "gridTemplateColumns": data.map(i => "1fr").join(" ") }}>
-            {data.map(article => (
-                <section key={article._id} className={styles.card}>
+            {data.map(article => {
+                if (!article)
+                    return null
+
+                return <section key={article._id} className={styles.card}>
                     <img className={styles.image} src={urlFor(article?.mainImage?.image).url()} />
                     <span className={styles.category}>Lær mer</span>
-                    <h2 className={styles.title}>{article.title}</h2>
+                    <h2 className={styles.title}>{article?.title}</h2>
                 </section>
-            ))}
+            })}
         </article>
     )
 }
--- unil_web/components/recipeBlock/recipeBlock.js
+++ unil_web/components/recipeBlock/recipeBlock.js
@@ -2,19 +2,29 @@ import styles from "../../styles/RecipeBlock.module.css"
 import { urlFor } from "../../lib/sanity"
 
 export default function RecipeBlock({ data }) {
+
+    if (!data)
+        return null
+
+
     return (
         <article className={styles.main} style={{ "gridTemplateColumns": data.map(i => "1fr").join(" ") }}>
-            {data.map(recipe => (
-                <section key={recipe._id} className={styles.card}>
+            {data.length > 0 && data.map(recipe => {
+                if (!recipe)
+                    return null
+
+                return <section key={recipe._id} className={styles.card}>
                     <a href={`/recipe/${recipe?.slug?.current}`} className={styles.link}>
                         <img className={styles.image} src={urlFor(recipe?.recipeImage).url()} />
                         <div className={styles.text}>
-                            <h1>{recipe.title}</h1>
-                            <span className={styles.time}>{recipe.timeEstiamte}</span>
+                            <h1>{recipe?.title}</h1>
+                            <span className={styles.time}>{recipe?.timeEstiamte}</span>
                         </div>
                     </a>
                 </section>
-            ))}
+            }
+
+            )}
         </article>
     )
 }
